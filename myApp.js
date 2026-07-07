d62569c83506fbd79b0822c116575e01323dda15
chopperwd-collab <chopperwd@gmail.com>
2026-07-07 03:12:24 +0000

    updated myApp.js with no extra code

--- myApp.js
+++ myApp.js
@@ -2,28 +2,19 @@ const express = require('express');
 const app = express();
 const helmet = require('helmet');
 
-// 1. Middleware setup
 app.use(helmet());
-app.disable('strict-transport-security');
 app.use(express.static('public'));
 
-// 2. API Routes
 const api = require('./server.js');
 app.use('/_api', api);
 
-// 3. Main Route
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-// 4. Server Listener (Keep this last)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
 
-// 5. Final Export
 module.exports = app;
-
-
-// depoly test
